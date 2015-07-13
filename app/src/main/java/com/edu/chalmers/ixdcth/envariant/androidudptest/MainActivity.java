358af7bd3a63fcee1e2689e610ba3f20b517a04b
joelha <joel.hammar@gmail.com>
2015-07-13 13:23:53 +0200

    Delay added, redid previous stuff according to fit task

--- app/src/main/java/com/edu/chalmers/ixdcth/envariant/androidudptest/MainActivity.java
+++ app/src/main/java/com/edu/chalmers/ixdcth/envariant/androidudptest/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import java.net.InetAddress;
@@ -27,11 +28,39 @@ public class MainActivity extends ActionBarActivity {
     //termination character for messages
     private static final String END = "\n";
 
+    private NumberPicker delayPicker;
+    //Needed since setMaxValue appears to be buggy..
+    private int delay = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadAddresses();
+        initDelayPicker();
+    }
+
+    /**
+     * initializes the numberpicker for selecting the delay of the messages. Sets the available
+     * values, max/min value and default value
+     */
+    private void initDelayPicker() {
+        delayPicker = (NumberPicker) findViewById(R.id.delayPicker);
+
+        String[] values = {"0", "100", "200", "300", "400", "500", "600", "700", "800", "900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000"};
+        delayPicker.setDisplayedValues(values);
+        delayPicker.setMaxValue(20);
+        delayPicker.setMinValue(0);
+        delayPicker.setValue(0);
+        delayPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                //wierd fix needed since getValue and setMaxValue of the numberpicker won't work,
+                // setMaxValue throws index out of bounds exception if larger than the length of the
+                // string array (even though this shouldn't have anyting to do with the index..)
+                delay = newVal * 100;
+            }
+        });
     }
 
     @Override
@@ -53,66 +82,61 @@ public class MainActivity extends ActionBarActivity {
         handler.setPort(port);
     }
 
-    public void onClickVideo1(View view) {
-        Toast.makeText(getApplicationContext(), "Playing video 1", Toast.LENGTH_SHORT).show();
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    //example sequence
-                    MessageHandler.getInstance().sendMessage("start video1" + END);
-                    sleep(500);
-                    MessageHandler.getInstance().sendMessage("stop video1" + END);
-                    MessageHandler.getInstance().sendMessage("start video2" + END);
-                    sleep(1200);
-                    MessageHandler.getInstance().sendMessage("start fade" + END);
-                    sleep(200);
-                    MessageHandler.getInstance().sendMessage("stop fade" + END);
-                    MessageHandler.getInstance().sendMessage("start video2" + END);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+    /**
+     * called whenever one of the start buttons are pressed
+     *
+     * @param view
+     */
+    public void onClickStart(View view) {
+        String msg = "";
+        if (view.equals(findViewById(R.id.startVideo1Button))) {
+            msg = "start video1" + END;
+        } else if (view.equals(findViewById(R.id.startVideo2Button))) {
+            msg = "start video2" + END;
+        } else if (view.equals(findViewById(R.id.startVideo3Button))) {
+            msg = "start video3" + END;
+        } else if (view.equals(findViewById(R.id.startFadeButton))) {
+            msg = "start fade" + END;
+        }
+        Log.d(DEBUG_TAG, msg);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        sendMessage(msg);
     }
 
-    public void onClickVideo2(View view) {
-        Toast.makeText(getApplicationContext(), "Playing video 2", Toast.LENGTH_SHORT).show();
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    //example sequence
-                    MessageHandler.getInstance().sendMessage("start video2" + END);
-                    sleep(500);
-                    MessageHandler.getInstance().sendMessage("start fade" + END);
-                    sleep(1200);
-                    MessageHandler.getInstance().sendMessage("stop fade" + END);
-                    sleep(200);
-                    MessageHandler.getInstance().sendMessage("start fade" + END);
-                    sleep(1200);
-                    MessageHandler.getInstance().sendMessage("stop fade" + END);
-                    MessageHandler.getInstance().sendMessage("stop video2" + END);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+    /**
+     * called whenever one of the stop buttons are pressed
+     *
+     * @param view
+     */
+    public void onClickStop(View view) {
+        String msg = "";
+        if (view.equals(findViewById(R.id.stopVideo1Button))) {
+            msg = "stop video1" + END;
+        } else if (view.equals(findViewById(R.id.stopVideo2Button))) {
+            msg = "stop video2" + END;
+        } else if (view.equals(findViewById(R.id.stopVideo3Button))) {
+            msg = "stop video3" + END;
+        } else if (view.equals(findViewById(R.id.stopFadeButton))) {
+            msg = "stop fade" + END;
+        }
+        Log.d(DEBUG_TAG, msg);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        sendMessage(msg);
     }
 
-    public void onClickVideo3(View view) {
-        Toast.makeText(getApplicationContext(), "Playing video 3", Toast.LENGTH_SHORT).show();
+    /**
+     * helper for sending messages, sends given string with currently picked delay.
+     *
+     * @param msg
+     */
+    private void sendMessage(String msg) {
+        final String message = msg;
         new Thread() {
             @Override
             public void run() {
                 try {
-                    //example sequence
-                    MessageHandler.getInstance().sendMessage("start video3" + END);
-                    sleep(500);
-                    MessageHandler.getInstance().sendMessage("start fade" + END);
-                    sleep(1000);
-                    MessageHandler.getInstance().sendMessage("stop fade" + END);
-                    MessageHandler.getInstance().sendMessage("stop video3" + END);
+                    sleep(delay);
+                    MessageHandler.getInstance().sendMessage(message);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
